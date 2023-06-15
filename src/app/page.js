c80103defe4cc1811a71f1df70099603c8a29f40
martindevaluado <pmartinacosta@gmail.com>
2023-06-15 20:23:34 -0300

    add: loop logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,73 @@
+"use client";
+
+import { useState } from "react";
+
 import Player from "./player";
 
 export default function Home() {
+  const [loop, setLoop] = useState(false);
+
   return (
-    <main className="grid grid-cols-1 lg:grid-cols-3 grid-flow-row gap-4 min-h-screen items-center p-24 select-none">
+    <main className="grid grid-cols-1 lg:grid-cols-3 grid-flow-row gap-8 min-h-screen items-center p-12 md:p-24 select-none">
+      <button onClick={() => setLoop(!loop)} className="text-xl font-semibold">
+        {loop ? "Loop Activado" : "Loop Desactivado"}
+      </button>
+
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Aplausos</h3>
-        <Player source="/sounds/claps.mp3" />
+        <Player source="/sounds/claps.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Crónica</h3>
-        <Player source="/sounds/cronica.mp3" />
+        <Player source="/sounds/cronica.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Datazo</h3>
-        <Player source="/sounds/datazo.mp3" />
+        <Player source="/sounds/datazo.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Grillo</h3>
-        <Player source="/sounds/grillo.mp3" />
+        <Player source="/sounds/grillo.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Meme créditos finales</h3>
-        <Player source="/sounds/meme-de-creditos-finales.mp3" />
+        <Player source="/sounds/meme-de-creditos-finales.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Ponele voluntad</h3>
-        <Player source="/sounds/ponele-voluntad.mp3" />
+        <Player source="/sounds/ponele-voluntad.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Sexy saxofon</h3>
-        <Player source="/sounds/sexy-saxofon.mp3" />
+        <Player source="/sounds/sexy-saxofon.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Violín triste</h3>
-        <Player source="/sounds/violin-triste.mp3" />
+        <Player source="/sounds/violin-triste.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Y se marchó</h3>
-        <Player source="/sounds/y-se-marcho.mp3" />
+        <Player source="/sounds/y-se-marcho.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Ahí estáaaaaa goool (Closs)</h3>
-        <Player source="/sounds/mariano-closs-ahi-esta-gol.mp3" />
+        <Player source="/sounds/mariano-closs-ahi-esta-gol.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Ahí estáaaaaa goool (Closs)</h3>
-        <Player source="/sounds/mariano-closs-ahi-esta-gol.mp3" />
+        <Player source="/sounds/mariano-closs-ahi-esta-gol.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Mision Imposible</h3>
-        <Player source="/sounds/mision-imposible.mp3" />
+        <Player source="/sounds/mision-imposible.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Misterio</h3>
-        <Player source="/sounds/misterio.mp3" />
+        <Player source="/sounds/misterio.mp3" loop={loop} />
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Titanic</h3>
-        <Player source="/sounds/titanic.mp3" />
+        <Player source="/sounds/titanic.mp3" loop={loop} />
       </div>
     </main>
   );
--- src/app/player/index.js
+++ src/app/player/index.js
@@ -2,11 +2,11 @@
 
 import { useRef } from "react";
 
-export default function Player({ source }) {
+export default function Player({ source, loop }) {
   const audioRef = useRef(null);
 
   return (
-    <audio controls id="tts-audio" ref={audioRef} loop>
+    <audio controls id="tts-audio" ref={audioRef} loop={loop}>
       <source src={source} type="audio/mpeg" />
     </audio>
   );
